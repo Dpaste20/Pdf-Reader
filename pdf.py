2bc428d2495e8b0c53ecb6ec85e129c0deb63c98
D20 <96135484+Dpaste20@users.noreply.github.com>
2022-11-29 12:04:22 +0530

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,28 @@
+# Pdf reader using python
+from PyPDF2 import PdfReader
+import pyttsx3
+
+engine = pyttsx3.init() 
+voices = engine.getProperty('voices')     
+engine.setProperty('voice', voices[1].id)
+
+rate = engine.getProperty('rate')                           #printing current voice rate
+engine.setProperty('rate', 180)
+
+def speak(a):
+    """Speak function"""
+    engine.say(a)
+    engine.runAndWait()
+
+
+reader = PdfReader("bitcoin.pdf")
+
+number_of_pages = reader.getNumPages()
+
+for pages in range(number_of_pages):
+    page = reader.pages[pages]
+    text = page.extract_text()
+    speak(text)
+
+
+
